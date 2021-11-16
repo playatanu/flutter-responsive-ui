d9f2c7e03ddccad5468323ad6b816e2fb627c509
Atanu Debnath <playatanuyt@gmail.com>
2021-11-16 14:26:00 +0530

    add new page phone page

---
+++ lib/phone_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PhoneMode extends StatelessWidget {
+  const PhoneMode({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
+  }
+}
